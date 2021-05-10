b86a8ae82e6c3fc4a1b6fe460f6ed967bab36b53
Mateusz Strojecki <ms@intercam-studios.com>
2021-05-10 16:21:08 +0200

    Simple layout

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="navbar navbar-expand-md navbar-light shadow-sm bg-success">
+        <div class="container">
+            Navigation container
+        </div>
+    </nav>
+</template>
